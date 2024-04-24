1710ad1b083eb48805162023cfe9d9816951a964
abdul-haadi <intern.abdulhaadi@gmail.com>
2024-04-24 14:31:48 +0530

    ProductManagementAPP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [productname, setProductName] = useState("");
+  const [specs, setSpecs] = useState("");
+  const [price, setPrice] = useState(0);
+  const [products, setProducts] = useState([]);
+  const [selectedId, setSelectedId] = useState(0);
+
+  function handleProductName(event) {
+    setProductName(event.target.value);
+  }
+
+  function handleSpecs(event) {
+    setSpecs(event.target.value);
+  }
+
+  function handlePrice(event) {
+    setPrice(event.target.value);
+  }
+
+  function handleAddProduct() {
+    setProducts((val) => {
+      return [
+        ...val,
+        {
+          id: val.length + 1,
+          productname: productname,
+          specs: specs,
+          price: price,
+        },
+      ];
+    });
+    setProductName("");
+    setPrice(0);
+    setSpecs("");
+  }
+
+  function handleDelete(index) {
+    let reducedList = [...products];
+    reducedList.splice(index, 1);
+    setProducts(reducedList);
+  }
+
+  function handleEdit(id) {
+    let product = products.find((product) => {
+      return product.id === id;
+    });
+    setProductName(product.productname);
+    setSpecs(product.specs);
+    setPrice(product.price);
+    setSelectedId(id);
+  }
+
+  function onEditProduct(id) {
+    setProducts((val) => {
+      return val.map((product) => {
+        if (product.id === id) {
+          return {
+            id: id,
+            productname: productname,
+            price: price,
+            specs: specs,
+          };
+        } else {
+          return product;
+        }
+      });
+    });
+    setSelectedId(0);
+    setProductName("");
+    setPrice(0);
+    setSpecs("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="product-management-application">
+      <div className="product-grid">
+        {products.map((product, index) => {
+          return (
+            <div className="product-card">
+              <span>Product Name: {product.productname}</span>
+              <span>Specification: {product.specs}</span>
+              <span>Price: {product.price}</span>
+              <div className="product-actions">
+                <button className="edit-btn" onClick={() => handleEdit(product.id)}>Edit</button>
+                <button className="delete-btn" onClick={() => handleDelete(index)}>Delete</button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="product-add-container">
+        <input
+          type="text"
+          placeholder="Product Name"
+          value={productname}
+          onChange={handleProductName}
+        ></input>
+        <input
+          type="text"
+          placeholder="Specification"
+          value={specs}
+          onChange={handleSpecs}
+        ></input>
+        <input
+          type="text"
+          placeholder="Price"
+          value={price}
+          onChange={handlePrice}
+        ></input>
+        {selectedId === 0 ? (
+          <button className="button add" onClick={handleAddProduct}>
+            Add
+          </button>
+        ) : (
+          <button
+            className="button edit"
+            onClick={() => onEditProduct(selectedId)}
+          >
+            Edit
+          </button>
+        )}
+      </div>
     </div>
   );
 }
